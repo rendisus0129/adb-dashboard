89546ab302f9492bb0e911df12a170b4a075bff0
rendisus0129 <rendisusanto8888@gmail.com>
2026-06-07 21:44:53 +0700

    Update script.js

--- script.js
+++ script.js
@@ -3,28 +3,28 @@ const adb = [
 {
 nama:"ADB DJAMIL",
 rayon:"RAYON A",
-foto:"foto/profil_icon.jpg",
+foto:"foto/profil_icon.png",
 link:"https://docs.google.com/spreadsheets/d/1ujfNqB6_tzoWpUZbix1rAqMXyxrLKX9RJ_Kk2DHM5C8/edit"
 },
 
 {
 nama:"ADB EGY",
 rayon:"RAYON A",
-foto:"foto/profil_icon.jpg",
+foto:"foto/profil_icon.png",
 link:"https://docs.google.com/spreadsheets/d/1uBgRqOy43sJ0SYbdvA-RyuC7j4ATPM9XjclkVa8CE7Y/edit"
 },
 
 {
 nama:"ADB SOPIAN HADI",
 rayon:"RAYON A",
-foto:"foto/profil_icon.jpg",
+foto:"foto/profil_icon.png",
 link:"https://docs.google.com/spreadsheets/d/1E7X9fKFzxCb2_DOo0ptmlINPrbG4i2Y04B9dsZ9QH3c/edit"
 },
 
 {
 nama:"ADB DIMAS",
 rayon:"RAYON A",
-foto:"foto/profil_icon.jpg",
+foto:"foto/profil_icon.png",
 link:"https://docs.google.com/spreadsheets/d/1AjG19Q98nxNgsXiDpyL3jsKMNORljmCNMDY7QVBlIso/edit"
 },
 
@@ -32,28 +32,28 @@ link:"https://docs.google.com/spreadsheets/d/1AjG19Q98nxNgsXiDpyL3jsKMNORljmCNMD
 {
 nama:"ADB AEF",
 rayon:"RAYON B",
-foto:"foto/profil_icon.jpg",
+foto:"foto/profil_icon.png",
 link:"https://docs.google.com/spreadsheets/d/1DBnPh15T0ki87S2Wh5f3Zu-qSVc7XrLdAe8fSNYgzoE/edit"
 },
 
 {
 nama:"ADB DIKI",
 rayon:"RAYON B",
-foto:"foto/profil_icon.jpg",
+foto:"foto/diki.png",
 link:"https://docs.google.com/spreadsheets/d/1j01Uw23LaU8_gqHLmiP4fX2w_1w-_hwBxuQT8MUtYo4/edit"
 },
 
 {
 nama:"ADB TEGUH",
 rayon:"RAYON B",
-foto:"foto/profil_icon.jpg",
+foto:"foto/profil_icon.png",
 link:"https://docs.google.com/spreadsheets/d/1PYS-cJR27tY3F6pWAl_w5Ej0aQ4RZ0BQTuxoJj-rEvg/edit"
 },
 
 {
 nama:"ADB SETIADI",
 rayon:"RAYON B",
-foto:"foto/profil_icon.jpg",
+foto:"foto/profil_icon.png",
 link:"https://docs.google.com/spreadsheets/d/1xfzMDOqwqlIjDWkXTkOB2QDE6h77gjeYMr7IqC4bmlA/edit"
 },
 
@@ -61,21 +61,21 @@ link:"https://docs.google.com/spreadsheets/d/1xfzMDOqwqlIjDWkXTkOB2QDE6h77gjeYMr
 {
 nama:"ADB DENI",
 rayon:"RAYON C",
-foto:"foto/profil_icon.jpg",
+foto:"foto/profil_icon.png",
 link:"https://docs.google.com/spreadsheets/d/1e1GgN8yPUiCBA7ITrOmTzOV00YXwiYKQFowUpabxSMs/edit"
 },
 
 {
 nama:"ADB ANWAR",
 rayon:"RAYON C",
-foto:"foto/profil_icon.jpg",
+foto:"foto/profil_icon.png",
 link:"https://docs.google.com/spreadsheets/d/1_Sc9I9_QMt2UDVH0W8lStwTnyYZeZPrjfM53SqE-2q0/edit"
 },
 
 {
 nama:"ADB RIKI",
 rayon:"RAYON C",
-foto:"foto/profil_icon.jpg",
+foto:"foto/profil_icon.png",
 link:"https://docs.google.com/spreadsheets/d/191RdvbivyMELQHlf9ucfeeahxIGx7oz8i5Qvk01uNws/edit"
 },
 
@@ -83,14 +83,14 @@ link:"https://docs.google.com/spreadsheets/d/191RdvbivyMELQHlf9ucfeeahxIGx7oz8i5
 {
 nama:"ADB HARVI",
 rayon:"RAYON D",
-foto:"foto/profil_icon.jpg",
+foto:"foto/profil_icon.png",
 link:"https://docs.google.com/spreadsheets/d/1fGEkt4StwG32hFMgtSiVW8FLmmLzcmGtwzpFeEOhSKc/edit"
 },
 
 {
 nama:"ADB ANDRI",
 rayon:"RAYON D",
-foto:"foto/profil_icon.jpg",
+foto:"foto/profil_icon.png",
 link:"https://docs.google.com/spreadsheets/d/1uVrwim4npMTcc_zWFZSpr5GOhMcbvgqiapWnLCt-o2g/edit"
 },
 
@@ -98,14 +98,14 @@ link:"https://docs.google.com/spreadsheets/d/1uVrwim4npMTcc_zWFZSpr5GOhMcbvgqiap
 {
 nama:"ADB ASEP",
 rayon:"RAYON KANVAS",
-foto:"foto/profil_icon.jpg",
+foto:"foto/profil_icon.png",
 link:"https://docs.google.com/spreadsheets/d/1BwyQZTWYoWEBKAjH8CGZsoEzOScxpFaH8VMhxr8xMxg/edit"
 },
 
 {
 nama:"ADB SUJANA",
 rayon:"RAYON KANVAS",
-foto:"foto/profil_icon.jpg",
+foto:"foto/profil_icon.png",
 link:"https://docs.google.com/spreadsheets/d/1q59kksy-sJlrD0TRmalZV8GpbPnbsQf1wI-YYqsH7L8/edit"
 }
 
@@ -119,77 +119,77 @@ const atss = [
 {
 nama:"ATSS ABDUL",
 rayon:"RAYON A",
-foto:"foto/profil_icon.jpg",
+foto:"foto/profil_icon.png",
 link:"#"
 },
 
 {
 nama:"ATSS HASAN",
 rayon:"RAYON A",
-foto:"foto/profil_icon.jpg",
+foto:"foto/profil_icon.png",
 link:"#"
 },
 
 {
 nama:"ATSS EPUL",
 rayon:"RAYON B",
-foto:"foto/profil_icon.jpg",
+foto:"foto/profil_icon.png",
 link:"#"
 },
 
 {
 nama:"ATSS BAUDIN",
 rayon:"RAYON B",
-foto:"foto/profil_icon.jpg",
+foto:"foto/profil_icon.png",
 link:"#"
 },
 
 {
 nama:"ATSS TOFA",
 rayon:"RAYON C",
-foto:"foto/profil_icon.jpg",
+foto:"foto/profil_icon.png",
 link:"#"
 },
 
 {
 nama:"ATSS NENDY",
 rayon:"RAYON C",
-foto:"foto/profil_icon.jpg",
+foto:"foto/profil_icon.png",
 link:"#"
 },
 
 {
 nama:"ATSS OPAN",
 rayon:"RAYON D",
-foto:"foto/profil_icon.jpg",
+foto:"foto/profil_icon.png",
 link:"#"
 },
 
 {
 nama:"ATSS YOGI",
 rayon:"RAYON D",
-foto:"foto/profil_icon.jpg",
+foto:"foto/profil_icon.png",
 link:"#"
 },
 
 {
 nama:"ATSS GALUH",
 rayon:"RAYON E",
-foto:"foto/profil_icon.jpg",
+foto:"foto/profil_icon.png",
 link:"#"
 },
 
 {
 nama:"ATSS SAMSUL",
 rayon:"RAYON E",
-foto:"foto/profil_icon.jpg",
+foto:"foto/profil_icon.png",
 link:"#"
 },
 
 {
 nama:"ATSS ERDI",
 rayon:"RAYON E",
-foto:"foto/profil_icon.jpg",
+foto:"foto/profil_icon.png",
 link:"#"
 }
 
@@ -211,7 +211,7 @@ html+=`
 <img 
 class="foto-sales"
 src="${x.foto}"
-onerror="this.src='foto/default.jpg'"
+onerror="this.src='foto/profil_icon.png'"
 >
 
 
