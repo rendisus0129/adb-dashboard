d83f445839da3c4e39044db28f4a6af5ad02aa83
rendisus0129 <rendisusanto8888@gmail.com>
2026-06-07 20:20:07 +0700

    Update script.js

--- script.js
+++ script.js
@@ -12,13 +12,24 @@ rayon:"RAYON A",
 link:"https://docs.google.com/spreadsheets/d/1uBgRqOy43sJ0SYbdvA-RyuC7j4ATPM9XjclkVa8CE7Y/edit"
 },
 
-
 {
 nama:"ADB SOPIAN HADI",
 rayon:"RAYON A",
 link:"https://docs.google.com/spreadsheets/d/1E7X9fKFzxCb2_DOo0ptmlINPrbG4i2Y04B9dsZ9QH3c/edit"
 },
 
+{
+nama:"ADB DIMAS",
+rayon:"RAYON A",
+link:"https://docs.google.com/spreadsheets/d/1AjG19Q98nxNgsXiDpyL3jsKMNORljmCNMDY7QVBlIso/edit"
+},
+
+
+{
+nama:"ADB AEF",
+rayon:"RAYON B",
+link:"https://docs.google.com/spreadsheets/d/1DBnPh15T0ki87S2Wh5f3Zu-qSVc7XrLdAe8fSNYgzoE/edit"
+},
 
 {
 nama:"ADB DIKI",
@@ -26,22 +37,145 @@ rayon:"RAYON B",
 link:"https://docs.google.com/spreadsheets/d/1j01Uw23LaU8_gqHLmiP4fX2w_1w-_hwBxuQT8MUtYo4/edit"
 },
 
-
 {
 nama:"ADB TEGUH",
 rayon:"RAYON B",
 link:"https://docs.google.com/spreadsheets/d/1PYS-cJR27tY3F6pWAl_w5Ej0aQ4RZ0BQTuxoJj-rEvg/edit"
+},
+
+{
+nama:"ADB SETIADI",
+rayon:"RAYON B",
+link:"https://docs.google.com/spreadsheets/d/1xfzMDOqwqlIjDWkXTkOB2QDE6h77gjeYMr7IqC4bmlA/edit"
+},
+
+
+{
+nama:"ADB DENI",
+rayon:"RAYON C",
+link:"https://docs.google.com/spreadsheets/d/1e1GgN8yPUiCBA7ITrOmTzOV00YXwiYKQFowUpabxSMs/edit"
+},
+
+{
+nama:"ADB ANWAR",
+rayon:"RAYON C",
+link:"https://docs.google.com/spreadsheets/d/1_Sc9I9_QMt2UDVH0W8lStwTnyYZeZPrjfM53SqE-2q0/edit"
+},
+
+{
+nama:"ADB RIKI",
+rayon:"RAYON C",
+link:"https://docs.google.com/spreadsheets/d/191RdvbivyMELQHlf9ucfeeahxIGx7oz8i5Qvk01uNws/edit"
+},
+
+
+{
+nama:"ADB HARVI",
+rayon:"RAYON D",
+link:"https://docs.google.com/spreadsheets/d/1fGEkt4StwG32hFMgtSiVW8FLmmLzcmGtwzpFeEOhSKc/edit"
+},
+
+{
+nama:"ADB ANDRI",
+rayon:"RAYON D",
+link:"https://docs.google.com/spreadsheets/d/1uVrwim4npMTcc_zWFZSpr5GOhMcbvgqiapWnLCt-o2g/edit"
+},
+
+
+{
+nama:"ADB ASEP",
+rayon:"RAYON KANVAS",
+link:"https://docs.google.com/spreadsheets/d/1BwyQZTWYoWEBKAjH8CGZsoEzOScxpFaH8VMhxr8xMxg/edit"
+},
+
+{
+nama:"ADB SUJANA",
+rayon:"RAYON KANVAS",
+link:"https://docs.google.com/spreadsheets/d/1q59kksy-sJlrD0TRmalZV8GpbPnbsQf1wI-YYqsH7L8/edit"
 }
 
 ];
 
 
 
-function load(){
+
+const atss = [
+
+{
+nama:"ATSS ABDUL",
+rayon:"RAYON A",
+link:"#"
+},
+
+{
+nama:"ATSS HASAN",
+rayon:"RAYON A",
+link:"#"
+},
+
+{
+nama:"ATSS EPUL",
+rayon:"RAYON B",
+link:"#"
+},
+
+{
+nama:"ATSS BAUDIN",
+rayon:"RAYON B",
+link:"#"
+},
+
+{
+nama:"ATSS TOFA",
+rayon:"RAYON C",
+link:"#"
+},
+
+{
+nama:"ATSS NENDY",
+rayon:"RAYON C",
+link:"#"
+},
+
+{
+nama:"ATSS OPAN",
+rayon:"RAYON D",
+link:"#"
+},
+
+{
+nama:"ATSS YOGI",
+rayon:"RAYON D",
+link:"#"
+},
+
+{
+nama:"ATSS GALUH",
+rayon:"RAYON E",
+link:"#"
+},
+
+{
+nama:"ATSS SAMSUL",
+rayon:"RAYON E",
+link:"#"
+},
+
+{
+nama:"ATSS ERDI",
+rayon:"RAYON E",
+link:"#"
+}
+
+];
+
+
+
+function render(data,id){
 
 let html="";
 
-adb.forEach(x=>{
+data.forEach(x=>{
 
 html+=`
 
@@ -51,13 +185,9 @@ html+=`
 
 <p>${x.rayon}</p>
 
-
 <a href="${x.link}" target="_blank">
-
 Lihat Aktivitas
-
 </a>
-
 
 </div>
 
@@ -66,9 +196,15 @@ Lihat Aktivitas
 });
 
 
-document.getElementById("adbList").innerHTML=html;
+document.getElementById(id).innerHTML=html;
 
 }
+
+
+
+render(adb,"adbList");
+
+render(atss,"atssList");
 
 
 
@@ -82,7 +218,3 @@ document.getElementById(id)
 .classList.remove("hide");
 
 }
-
-
-
-load();
